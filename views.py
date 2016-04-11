ac6d27154f3d961a7c5932cbd092cdef6126460f
Braden Jageman <neurobomber@gmail.com>
2016-04-10 19:25:07 -0700

    added blog comments (disqus)

--- views.py
+++ views.py
@@ -2,22 +2,30 @@ from blog.models import Blog, Category
 from django.shortcuts import render_to_response, get_object_or_404
 from django.views.decorators.csrf import csrf_protect
 from django.template.context import RequestContext
+from django.conf import settings
+
+def setDisqus():
+    try:
+        return settings.DISQUSTAG
+    except AttributeError:
+        return None
 
 @csrf_protect
 def index(request):
-    return render_to_response('blog/index.html', RequestContext(request,
-            {
-            'categories': Category.objects.all(),
-            'posts': Blog.objects.all().order_by('-posted', 'title')[:5],
-            }))
+    args = {
+        'categories': Category.objects.all(),
+        'posts': Blog.objects.all().order_by('-posted', 'title')[:5],
+    }
+    return render_to_response('blog/index.html', RequestContext(request, args))
 
 #Returns the (hopefully) singular post for that blog
-def view_post(request, slug):   
-    return render_to_response('blog/index.html', RequestContext(request,
-            {
-            'posts': Blog.objects.filter(slug=slug).order_by('-posted', 'title')[:5],
-            'slug': slug,
-            }))
+def view_post(request, slug):
+    args = {
+    'posts': Blog.objects.filter(slug=slug).order_by('-posted', 'title')[:5],
+    'slug': slug,
+    }
+    args['disqus'] = setDisqus()
+    return render_to_response('blog/index.html', RequestContext(request, args))
 
 #Grabs all posts with the matching foreign id of the category
 def view_category(request, category):
