e49e2333f69699282f022d14bc3594a7c9bed35c
Braden Jageman <bjageman@gmail.com>
2016-11-25 23:08:56 -0800

    finally commiting to CKEditor

--- models.py
+++ models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.db.models import permalink
 from django.contrib.auth.models import User
 
-#Markdown
-#from django_markdown.models import MarkdownField
+#CKEditor
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Blog(models.Model):
     author = models.ForeignKey(User, null=True)
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    body = models.TextField()
+    body = RichTextField()
     posted = models.DateTimeField(db_index=True, auto_now_add=True)
     category = models.ForeignKey('blog.Category', null=True, blank=True)
 
